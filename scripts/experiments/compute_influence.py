d5ee0f63675ceea1dc370afa2b2030425c03dcfc
jbrophy <jonathanbrophy47@gmail.com>
2021-07-25 20:48:10 -0700

    fixing flight_delays trex special case

--- scripts/experiments/compute_influence.py
+++ scripts/experiments/compute_influence.py
@@ -141,7 +141,7 @@ def main(args):
     params, hash_str = util.explainer_params_to_dict(args.method, vars(args))
 
     # special case
-    if args.dataset == 'flight_delays' nad args.method == 'trex':
+    if args.dataset == 'flight_delays' and  args.method == 'trex':
         params['atol'] = 1e-1
 
     # create output dir
