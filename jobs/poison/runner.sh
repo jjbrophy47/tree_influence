a3815bbceeed4c9ffa170ef4c098f42887222762
jbrophy <jonathanbrophy47@gmail.com>
2021-09-07 11:26:29 -0700

    updating poison batch

--- jobs/poison/runner.sh
+++ jobs/poison/runner.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
+#SBATCH --job-name=Poison
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --account=uoml
 module load miniconda
 conda activate jbrophy-20210713
 
-dataset=$1
-tree_type=$2
-method=$3
-trunc_frac=$4
-update_set=$5
+tree_type=$1
+method=$2
+
+. jobs/config.sh
+
+dataset=${datasets[${SLURM_ARRAY_TASK_ID}]}
 
 python3 scripts/experiments/poison.py \
   --dataset $dataset \
   --tree_type $tree_type \
   --method $method \
-  --trunc_frac $trunc_frac \
-  --update_set $update_set \
