2c00e050c3c1fa42a2fbc2461b055ec6a77c4905
jbrophy <jonathanbrophy47@gmail.com>
2021-09-27 16:18:32 -0700

    added some notes about cb

--- jobs/inf/batch.sh
+++ jobs/inf/batch.sh
@@ -33,5 +33,13 @@ sbatch -a 7,15 -c 28 -t 2000 -p 'long'  -o ${o}${t}'_loo-%a.out'        $run $t 
 sbatch -a 9    -c 28 -t 4320 -p 'long'  -o ${o}${t}'_loo-%a.out'        $run $t 'loo'
 
 # scratch pad
-sbatch -a 3    -c 11 -t 1440 -p 'short' -o ${o}${t}'_leaf_inf-%a.out'   $run $t 'leaf_inf'
-sbatch -a 3,13 -c 11 -t 1440 -p 'short' -o ${o}${t}'_leaf_refit-%a.out' $run $t 'leaf_refit'
+sbatch -a 17-20                -c 11 -t 1440 -p 'short' -o ${o}${t}'_random-%a.out'     $run $t 'random'
+sbatch -a 2,5,7,11-15,17,19-21 -c 11 -t 1440 -p 'short' -o ${o}${t}'_target-%a.out'     $run $t 'target'
+sbatch -a 2-3,9,11-21          -c 11 -t 1440 -p 'short' -o ${o}${t}'_boostin-%a.out'    $run $t 'boostin'
+sbatch -a 1-3,7,9,13-19,20-21  -c 11 -t 1440 -p 'short' -o ${o}${t}'_input_sim-%a.out'  $run $t 'input_sim'
+sbatch -a 1-3,7-9,11-21        -c 11 -t 1440 -p 'short' -o ${o}${t}'_leaf_sim-%a.out'   $run $t 'leaf_sim'
+sbatch -a 1-3,5,7-9,11-21      -c 11 -t 1440 -p 'short' -o ${o}${t}'_leaf_infSP-%a.out' $run $t 'leaf_infSP'
+sbatch -a 1-21                 -c 11 -t 1440 -p 'short' -o ${o}${t}'_subsample-%a.out'  $run $t 'subsample'
+sbatch -a 1-21                 -c 11 -t 1440 -p 'short' -o ${o}${t}'_loo-%a.out'        $run $t 'loo'
+sbatch -a 3-6,8,10-13,16,18-21 -c 28 -t 1440 -p 'short' -o ${o}${t}'_leaf_inf-%a.out'   $run $t 'leaf_inf'
+sbatch -a 3-6,8,10-13,16,18-21 -c 28 -t 1440 -p 'short' -o ${o}${t}'_leaf_refit-%a.out' $run $t 'leaf_refit'
