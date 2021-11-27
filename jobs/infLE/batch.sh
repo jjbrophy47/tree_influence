47348aad887dfb2b769cb3a08c132fd10f6b678e
jbrophy <jonathanbrophy47@gmail.com>
2021-11-26 21:32:38 -0800

    added infLE jobs

---
+++ jobs/infLE/batch.sh
@@ -0,0 +1,37 @@
+
+run='jobs/infLE/runner.sh'
+o='jobs/logs/infLE/'
+t='lgb'
+
+sbatch -a 1-21  -c 3  -t 1440 -p 'short' -o ${o}${t}'_boostinLE-%a.out'    $run $t 'boostinLE'
+sbatch -a 1-21  -c 3  -t 1440 -p 'short' -o ${o}${t}'_leaf_infSPLE-%a.out' $run $t 'leaf_infSPLE'
+sbatch -a 1-21  -c 5  -t 1440 -p 'short' -o ${o}${t}'_looLE-%a.out'        $run $t 'looLE'
+sbatch -a 3-6,8,10-13,16,18-19,21 -c 11 -t 1440 -p 'short' -o ${o}${t}'_leaf_refitLE-%a.out' $run $t 'leaf_refitLE'
+sbatch -a 3-6,8,10-13,16,18-19,21 -c 11 -t 1440 -p 'short' -o ${o}${t}'_leaf_infLE-%a.out'   $run $t 'leaf_infLE'
+sbatch -a 1-21  -c 3  -t 1440 -p 'short' -o ${o}${t}'_boostinLEW1-%a.out'    $run $t 'boostinLEW1'
+sbatch -a 1-21  -c 3  -t 1440 -p 'short' -o ${o}${t}'_boostinLEW2-%a.out'    $run $t 'boostinLEW2'
+
+sbatch -a 22 -c 11 -t 1440 -p 'short' -o ${o}${t}'_boostinLE-%a.out'    $run $t 'boostinLE'
+sbatch -a 22 -c 11 -t 1440 -p 'short' -o ${o}${t}'_leaf_infSPLE-%a.out' $run $t 'leaf_infSPLE'
+sbatch -a 22 -c 28 -t 1440 -p 'short' -o ${o}${t}'_looLE-%a.out'        $run $t 'looLE'
+sbatch -a 22 -c 11 -t 1440 -p 'short' -o ${o}${t}'_boostinW1-%a.out'    $run $t 'boostinW1'
+sbatch -a 22 -c 11 -t 1440 -p 'short' -o ${o}${t}'_boostinW2-%a.out'    $run $t 'boostinW2'
+
+# xgb only
+sbatch -a 7,9,14,22 -c 28 -t 4320 -p 'long'  -o ${o}${t}'_looLE-%a.out'        $run $t 'looLE'
+
+# sgb only
+sbatch -a 7,15 -c 28 -t 2000  -p 'long'  -o ${o}${t}'_looLE-%a.out'        $run $t 'looLE'
+sbatch -a 9    -c 28 -t 10080 -p 'long'  -o ${o}${t}'_looLE-%a.out'        $run $t 'looLE'
+
+# cb only
+sbatch -a 6,11,19          -c 28 -t 2880  -p 'long'  -o ${o}${t}'_leaf_infLE-%a.out'   $run $t 'leaf_infLE'
+sbatch -a 3                -c 28 -t 10080 -p 'short' -o ${o}${t}'_leaf_infLE-%a.out'   $run $t 'leaf_infLE'
+sbatch -a 6,11,19          -c 28 -t 2880  -p 'long'  -o ${o}${t}'_leaf_refitLE-%a.out' $run $t 'leaf_refitLE'
+sbatch -a 3                -c 28 -t 10080 -p 'short' -o ${o}${t}'_leaf_refitLE-%a.out' $run $t 'leaf_refitLE'
+sbatch -a 3,6,12,18,21     -c 28 -t 2880  -p 'long'  -o ${o}${t}'_looLE-%a.out'        $run $t 'looLE'
+sbatch -a 13,16,19         -c 28 -t 4320  -p 'long'  -o ${o}${t}'_looLE-%a.out'        $run $t 'looLE'
+sbatch -a 1,17             -c 28 -t 10080 -p 'long'  -o ${o}${t}'_looLE-%a.out'        $run $t 'looLE'
+sbatch -a 2,11,20          -c 28 -t 10080 -p 'long'  -o ${o}${t}'_looLE-%a.out'        $run $t 'looLE'
+
+# scratch pad
---
+++ jobs/infLE/runner.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=InfLE
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --account=uoml
+module load miniconda
+conda activate jbrophy-20210713
+
+tree_type=$1
+method=$2
+
+. jobs/config.sh
+
+dataset=${datasets[${SLURM_ARRAY_TASK_ID}]}
+
+python3 scripts/experiments/single_test/influenceLE.py \
+  --dataset $dataset \
+  --tree_type $tree_type \
+  --method $method \
