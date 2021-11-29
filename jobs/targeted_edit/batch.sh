632ca8b60b740d5751b46282283569574de12e36
jbrophy <jonathanbrophy47@gmail.com>
2021-11-29 13:55:02 -0800

    updated batch script for targeted_edit

--- jobs/targeted_edit/batch.sh
+++ jobs/targeted_edit/batch.sh
@@ -3,32 +3,51 @@ run=jobs/targeted_edit/runner.sh
 o=jobs/logs/targeted_edit/
 t='lgb'
 
-sbatch -a 1-22  -c 20 -t 1440 -p 'short' -o ${o}${t}'_random-%a.out'       $run $t 'random'
-sbatch -a 1-22  -c 20 -t 1440 -p 'short' -o ${o}${t}'_leaf_sim-%a.out'     $run $t 'leaf_sim'
-sbatch -a 1-22  -c 20 -t 1440 -p 'short' -o ${o}${t}'_trex-%a.out'         $run $t 'trex'
-sbatch -a 1-22  -c 20 -t 1440 -p 'short' -o ${o}${t}'_boostin-%a.out'      $run $t 'boostin'
-sbatch -a 1-22  -c 20 -t 1440 -p 'short' -o ${o}${t}'_boostinW1-%a.out'    $run $t 'boostinW1'
-sbatch -a 1-22  -c 20 -t 1440 -p 'short' -o ${o}${t}'_boostinW2-%a.out'    $run $t 'boostinW2'
-sbatch -a 1-22  -c 20 -t 1440 -p 'short' -o ${o}${t}'_boostinLE-%a.out'    $run $t 'boostinLE'
-sbatch -a 1-22  -c 20 -t 1440 -p 'short' -o ${o}${t}'_boostinLEW1-%a.out'  $run $t 'boostinLEW1'
-sbatch -a 1-22  -c 20 -t 1440 -p 'short' -o ${o}${t}'_boostinLEW2-%a.out'  $run $t 'boostinLEW2'
+sbatch -a 1-21  -c 15 -t 1440 -p 'short' -o ${o}${t}'_random-%a.out'       $run $t 'random'
+sbatch -a 1-21  -c 15 -t 1440 -p 'short' -o ${o}${t}'_target-%a.out'       $run $t 'target'
+sbatch -a 1-21  -c 15 -t 1440 -p 'short' -o ${o}${t}'_leaf_sim-%a.out'     $run $t 'leaf_sim'
+sbatch -a 1-21  -c 15 -t 1440 -p 'short' -o ${o}${t}'_trex-%a.out'         $run $t 'trex'
+sbatch -a 1-21  -c 15 -t 1440 -p 'short' -o ${o}${t}'_boostin-%a.out'      $run $t 'boostin'
+sbatch -a 1-21  -c 15 -t 1440 -p 'short' -o ${o}${t}'_boostinW1-%a.out'    $run $t 'boostinW1'
+sbatch -a 1-21  -c 15 -t 1440 -p 'short' -o ${o}${t}'_boostinW2-%a.out'    $run $t 'boostinW2'
+sbatch -a 1-21  -c 15 -t 1440 -p 'short' -o ${o}${t}'_leaf_infSP-%a.out'   $run $t 'leaf_infSP'
+sbatch -a 1-21  -c 15 -t 1440 -p 'short' -o ${o}${t}'_loo-%a.out'          $run $t 'loo'
+sbatch -a 1-21  -c 15 -t 1440 -p 'short' -o ${o}${t}'_subsample-%a.out'    $run $t 'subsample'
+sbatch -a 3-6,8,10-13,16,18-19,21 -c 15 -t 1440 -p 'short' -o ${o}${t}'_leaf_inf-%a.out'   $run $t 'leaf_inf'
+sbatch -a 3-6,8,10-13,16,18-19,21 -c 15 -t 1440 -p 'short' -o ${o}${t}'_leaf_refit-%a.out' $run $t 'leaf_refit'
+
+sbatch -a 1-21  -c 15 -t 1440 -p 'short' -o ${o}${t}'_boostinLE-%a.out'    $run $t 'boostinLE'
+sbatch -a 1-21  -c 15 -t 1440 -p 'short' -o ${o}${t}'_boostinLEW1-%a.out'  $run $t 'boostinLEW1'
+sbatch -a 1-21  -c 15 -t 1440 -p 'short' -o ${o}${t}'_boostinLEW2-%a.out'  $run $t 'boostinLEW2'
+sbatch -a 1-21  -c 15 -t 1440 -p 'short' -o ${o}${t}'_leaf_infSPLE-%a.out' $run $t 'leaf_infSPLE'
+sbatch -a 1-21  -c 15 -t 1440 -p 'short' -o ${o}${t}'_looLE-%a.out'        $run $t 'looLE'
+sbatch -a 3-6,8,10-13,16,18-19,21 -c 15 -t 1440 -p 'short' -o ${o}${t}'_leaf_infLE-%a.out'   $run $t 'leaf_infLE'
+sbatch -a 3-6,8,10-13,16,18-19,21 -c 15 -t 1440 -p 'short' -o ${o}${t}'_leaf_refitLE-%a.out' $run $t 'leaf_refitLE'
+
+
+sbatch -a 22 -c 20 -t 1440 -p 'short' -o ${o}${t}'_random-%a.out'       $run $t 'random'
+sbatch -a 22 -c 20 -t 1440 -p 'short' -o ${o}${t}'_target-%a.out'       $run $t 'target'
+sbatch -a 22 -c 20 -t 1440 -p 'short' -o ${o}${t}'_leaf_sim-%a.out'     $run $t 'leaf_sim'
+sbatch -a 22 -c 20 -t 1440 -p 'short' -o ${o}${t}'_trex-%a.out'         $run $t 'trex'
+sbatch -a 22 -c 20 -t 1440 -p 'short' -o ${o}${t}'_boostin-%a.out'      $run $t 'boostin'
+sbatch -a 22 -c 20 -t 1440 -p 'short' -o ${o}${t}'_boostinW1-%a.out'    $run $t 'boostinW1'
+sbatch -a 22 -c 20 -t 1440 -p 'short' -o ${o}${t}'_boostinW2-%a.out'    $run $t 'boostinW2'
+sbatch -a 22 -c 20 -t 1440 -p 'short' -o ${o}${t}'_leaf_infSP-%a.out'   $run $t 'leaf_infSP'
+sbatch -a 22 -c 20 -t 1440 -p 'short' -o ${o}${t}'_loo-%a.out'          $run $t 'loo'
+sbatch -a 22 -c 20 -t 1440 -p 'short' -o ${o}${t}'_subsample-%a.out'    $run $t 'subsample'
+
+sbatch -a 22 -c 20 -t 1440 -p 'short' -o ${o}${t}'_boostinLE-%a.out'    $run $t 'boostinLE'
+sbatch -a 22 -c 20 -t 1440 -p 'short' -o ${o}${t}'_boostinLEW1-%a.out'  $run $t 'boostinLEW1'
+sbatch -a 22 -c 20 -t 1440 -p 'short' -o ${o}${t}'_boostinLEW2-%a.out'  $run $t 'boostinLEW2'
+sbatch -a 22 -c 20 -t 1440 -p 'short' -o ${o}${t}'_leaf_infSPLE-%a.out' $run $t 'leaf_infSPLE'
+sbatch -a 22 -c 20 -t 1440 -p 'short' -o ${o}${t}'_looLE-%a.out'        $run $t 'looLE'
 
 # LGB only
-sbatch -a 7,9,17,22          -c 1 --mem=60GB -t 1440 -p 'short' -o ${o}${t}'_trex-%a.out' $run $t 'trex'
-sbatch -a 15                 -c 1 --mem=60GB -t 2880 -p 'long'  -o ${o}${t}'_trex-%a.out' $run $t 'trex'
 
 # SGB only
-sbatch -a 7,17,22            -c 1 --mem=60GB -t 1440 -p 'short' -o ${o}${t}'_trex-%a.out' $run $t 'trex'
-sbatch -a 9                  -c 1 --mem=60GB -t 2880 -p 'long'  -o ${o}${t}'_trex-%a.out' $run $t 'trex'
-sbatch -a 15                 -c 1 --mem=60GB -t 4320 -p 'long'  -o ${o}${t}'_trex-%a.out' $run $t 'trex'
 
 # XGB only
-sbatch -a 9,17,22            -c 1 --mem=60GB -t 1440 -p 'short' -o ${o}${t}'_trex-%a.out' $run $t 'trex'
-sbatch -a 15                 -c 1 --mem=60GB -t 4320 -p 'long'  -o ${o}${t}'_trex-%a.out' $run $t 'trex'
 
 # CB only
-sbatch -a 2,17               -c 1 --mem=60GB -t 4320 -p 'long'  -o ${o}${t}'_trex-%a.out' $run $t 'trex'
-sbatch -a 7,9,14-15          -c 1 --mem=60GB -t 4320 -p 'long'  -o ${o}${t}'_trex-%a.out' $run $t 'trex'
-sbatch -a 22                 -c 1 --mem=60GB -t 7200 -p 'long'  -o ${o}${t}'_trex-%a.out' $run $t 'trex'
 
 # scratch pad
