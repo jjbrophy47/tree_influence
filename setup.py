8baff2b521549ea5902f13817bac4624512c78db
Jonathan Brophy <jonathanbrophy47@gmail.com>
2023-01-31 09:34:42 -0800

    updating version number

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if os.name == 'posix':
     libraries.append('m')
 
 setup(name="tree-influence",
-      version="0.1.1",
+      version="0.1.2",
       description="Influence Estimation for Gradient-Boosted Decision Trees",
       author="Jonathan Brophy",
       author_email="jonathanbrophy47@gmail.com",
